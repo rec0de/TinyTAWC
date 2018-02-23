c4d48bc44065dbe624c8d496558e02ba862d8b4b
rec0de <mail@rec0de.net>
2018-02-23 19:21:42 +0100

    Add license to main script, remove unnecessary slashes

--- ttawc.rb
+++ ttawc.rb
@@ -1,4 +1,27 @@
 #!/usr/bin/env ruby
+=begin
+MIT License
+
+Copyright (c) 2018 rec0de
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+=end
 
 def log(msg)
 	STDERR.puts '[Debug] ' + msg if $debug
@@ -17,7 +40,7 @@ helptext = ['Usage:', 'ruby ttawc.rb [options] [dictionary] [input file]', 'If n
 			'Options:',
 			'--raw (-r) Use raw input data with no sanitizing',
 			'--include="cat0,cat1,..." Include only the given categories',
-			'--exclude=\"cat0,cat1,...\" Include everything except the given categories',
+			'--exclude="cat0,cat1,..." Include everything except the given categories',
 			'--human Show human-readable output',
 			'--percent (-p) Show output in percent of total words',
 			'--sort (-s) Sort output by count (desc)',
