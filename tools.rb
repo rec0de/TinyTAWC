f3026c9fd7f4d1246884dc0a38042ae605386d6b
Kumiho <kumiho@rec0de.net>
2018-02-24 18:37:35 +0100

    Added utility script

---
+++ tools.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/env ruby
+
+def log(msg)
+	STDERR.puts '[Debug] ' + msg if $debug
+end
+
+def error(msg, warn = false)
+	STDERR.puts (warn ? '[Warning] ' : '[Error] ') + msg
+end
+
+def sanitize(text)
+	# Regex used to discard non-matching characters
+	if RUBY_VERSION < '2.3' then
+		error('[Warning] You are running an old version of Ruby. Parsing files with non-english characters (Accents, Umlauts, Chinese characters, ...) might not produce useful results when not using --raw', true)
+		inputalphabet = /[^A-Za-zöäüß]/ #[:word:] for word characters does not work in old Ruby versions, use english alphabet + german umlauts as a fallback
+	else
+		inputalphabet = Regexp.new('[^[:word:]]') # discard all non-word characters
+	end
+		
+	return text.gsub(inputalphabet, ' ').gsub(/[0-9]/, ' ').gsub(/\s+/, ' ')
+end
+
+def clean()
+	# Remove all options from ARGV
+	args = ARGV.select{ |a| (a[0] != 45 && a[0] != '-')} # 45 = "-", weird code to support Ruby 1.8.7
+
+	if args.length == 1 then
+		data = STDIN.read()
+	elsif args.length == 2 then
+		if File.exists?(args[1]) && File.readable?(args[1])
+			data = File.read(args[1])
+		else
+			error('Input file does not exist or is not readable')
+			exit
+		end
+	else
+		error('Too many arguments - clean expects one input file or STDIN')
+		exit
+	end
+
+	return sanitize(data)
+end
+
+def combine()
+	result = ''
+
+	# Remove all options from ARGV and remove first argument
+	args = ARGV.select{ |a| (a[0] != 45 && a[0] != '-')} # 45 = "-", weird code to support Ruby 1.8.7
+	args = args[1...args.length]
+
+	args.each{|path|
+		if File.exists?(path) && File.readable?(path)
+			result += path.gsub(/(.*\/|.*\\|\s)/, '') + ' ' + sanitize(File.read(path)) + "\n"
+		else
+			error('Input file does not exist or is not readable')
+			exit
+		end
+	}
+
+	return result
+end
+
+helptext = ['Usage:', 'ruby tools.rb [options] [mode] [input file/s]', 'If no input file is given, input data is read from STDIN', '',
+			'Modes:',
+			'clean Sanitizes the input file the same way TinyTAWC would',
+			'combine Combine the given input files into a line-based one (Does not support STDIN)',
+			'',
+			'Options:',
+			'--verbose (-d) Show debug information',
+			'--help (-h) Show this help and exit',
+			''
+		]
+
+$debug = false
+
+# Parse command line options
+ARGV.each do|a|
+	if a == '-d' or a == '--verbose' then
+		$debug = true
+	elsif a == '--human' then
+		$human = true
+	elsif a == '-p' or a == '--percent' then
+		$percent = true
+	elsif a == '-s' or a == '--sort' then
+		$sort = true
+	elsif a == '-r' or a == '--raw' then
+		$sanitize = false
+	elsif a == '-h' or a == '--help' then
+		helptext.each{|line| puts line}
+		exit
+	elsif a == 'clean' then
+		puts clean()
+		exit
+	elsif a == 'combine' then
+		puts combine()
+		exit
+	elsif a[0] == '-' then
+		error("Unknown option '"+a+"'")
+		exit
+	end
+end
